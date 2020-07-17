62dbc5dea5edb9aa75b6a30e0fb973125b83b1c9
T0person <59031052+T0person@users.noreply.github.com>
2020-07-17 17:35:03 +0300

    Create MVC
    Шаблонное приложение с использованием базы данных на MVC

---
+++ Login2/Login2/Data/PersonContext.cs
@@ -0,0 +1,18 @@
+﻿using Login2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login2.Data
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
